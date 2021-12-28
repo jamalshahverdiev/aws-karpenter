acba107d690edb2f85b70c0bc28d3ac687f0adb7
Jamal Shahverdiyev <jamal.shahverdiev@gmail.com>
2021-12-28 12:56:56 +0400

    Latest changes

--- generate_karpenter_provisioner_lt.sh
+++ generate_karpenter_provisioner_lt.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 cluster_yaml_script='generate_cluster_yaml.sh'
+region_name='us-east-2'
 userdata_template_file='userdata-encode-template.txt'
 karpenter_launchtemp_template_file='karpenter_launchtemp_template.yml'
 key_name=$(cat ${cluster_yaml_script} | grep karpenter_key_name | head -n1 | cut -f2 -d'=' | tr -d "'")
@@ -12,7 +13,8 @@ AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 ami_to_set='ami-04635d3effed08298'
 cluster_yaml_file=$(cat ${cluster_yaml_script} | grep cluster_template_output | head -n1 | awk '{ print $1 }' | cut -f2 -d'=' | tr -d "'")
 CLUSTER_NAME=$(cat ${cluster_yaml_file}| grep -A1 metadata | tail -n1 | awk '{ print $(NF)}')
-cluster_vpc_object=$(aws eks describe-cluster --name ${CLUSTER_NAME} --query 'cluster.resourcesVpcConfig')
+cluster_vpc_object=$(aws eks describe-cluster --region ${region_name} \
+    --name ${CLUSTER_NAME} --query 'cluster.resourcesVpcConfig')
 CLUSTER_VPCID_GET=$(echo $cluster_vpc_object | jq -r '.vpcId')
 provisioner_file='karpenter_provisioner.yml'
 DEPLOYMENT_FILE='inflate_deployment.yaml'
@@ -29,9 +31,13 @@ prepare_userdata_file(){
     userdata_output='userdata_output.txt' && rm ${userdata_output}
     cp ${userdata_template_input} ${userdata_output} 
     CLUSTER_NAME=$(cat ${cluster_yaml_file}| grep -A1 metadata | tail -n1 | awk '{ print $(NF)}')
-    API_SERVER_URL=$(aws eks describe-cluster --name ${CLUSTER_NAME} --query 'cluster.[endpoint,certificateAuthority][0]' --output text)
-    B64_CLUSTER_CA=$(aws eks describe-cluster --name ${CLUSTER_NAME} --query 'cluster.[endpoint,certificateAuthority][1]' --output text)
-    sed -i "s/replace_cluster_name/${CLUSTER_NAME}/g;s|replace_kubeapiurl|${API_SERVER_URL}|g;s/replace_cluster_ca/${B64_CLUSTER_CA}/g" ${userdata_output}
+    API_SERVER_URL=$(aws eks describe-cluster --region ${region_name} \
+        --name ${CLUSTER_NAME} --query 'cluster.[endpoint,certificateAuthority][0]' --output text)
+    B64_CLUSTER_CA=$(aws eks describe-cluster --region ${region_name} \
+        --name ${CLUSTER_NAME} --query 'cluster.[endpoint,certificateAuthority][1]' --output text)
+    sed -i "s/replace_cluster_name/${CLUSTER_NAME}/g;\
+        s|replace_kubeapiurl|${API_SERVER_URL}|g;\
+        s/replace_cluster_ca/${B64_CLUSTER_CA}/g" ${userdata_output}
     # cat ${userdata_output} 
     b64_encoded_userdata=$(cat ${userdata_output}| base64 | tr -d "\n")
     # echo ${b64_encoded_userdata} | base64 -d > zibil.txt
@@ -40,14 +46,24 @@ prepare_userdata_file(){
 prepare_security_group_rules(){
     CLUSTER_ADDITIONAL_SG=$(echo ${cluster_vpc_object}| jq -r '.securityGroupIds[]')
     CLUSTER_SG_GET=$(echo ${cluster_vpc_object} | jq -r '.clusterSecurityGroupId')
-    CLUSTER_SG_RULE_RESULT=$(aws ec2 authorize-security-group-ingress --group-id $CLUSTER_SG_GET --protocol all --port all --source-group $CREATE_GET_SECURTY_GROUP_ID)
-    CLUSTER_ADDITIONAL_SG_RULE_RESULT=$(aws ec2 authorize-security-group-ingress --group-id $CLUSTER_ADDITIONAL_SG --protocol tcp --port 443 --source-group $CREATE_GET_SECURTY_GROUP_ID)
+    CLUSTER_SG_RULE_RESULT=$(aws ec2 authorize-security-group-ingress --region ${region_name} \
+        --group-id $CLUSTER_SG_GET \
+        --protocol all --port all --source-group $CREATE_GET_SECURTY_GROUP_ID)
+    CLUSTER_ADDITIONAL_SG_RULE_RESULT=$(aws ec2 authorize-security-group-ingress --region ${region_name} \
+        --group-id $CLUSTER_ADDITIONAL_SG --protocol tcp --port 443 --source-group $CREATE_GET_SECURTY_GROUP_ID)
     for secur_gr_igress in ${CLUSTER_ADDITIONAL_SG} ${CLUSTER_SG_GET}; do 
-        aws ec2 authorize-security-group-ingress --group-id ${CREATE_GET_SECURTY_GROUP_ID} --protocol all --port all --source-group ${secur_gr_igress} &
+        aws ec2 authorize-security-group-ingress --region ${region_name} \
+            --group-id ${CREATE_GET_SECURTY_GROUP_ID} \
+            --protocol all --port all --source-group ${secur_gr_igress} &
     done
-    aws ec2 authorize-security-group-ingress --group-id ${CREATE_GET_SECURTY_GROUP_ID} --protocol all --port all --cidr 10.0.0.0/8 &
+    aws ec2 authorize-security-group-ingress --region ${region_name} \
+            --group-id ${CREATE_GET_SECURTY_GROUP_ID} \
+            --protocol all --port all --cidr 10.0.0.0/8 &
     for secur_gr_egress in '443' '1025-65535'; do
-        aws ec2 authorize-security-group-egress --group-id $CLUSTER_ADDITIONAL_SG --protocol tcp --port ${secur_gr_egress} --source-group $CREATE_GET_SECURTY_GROUP_ID &
+        aws ec2 authorize-security-group-egress --region ${region_name} \
+            --group-id $CLUSTER_ADDITIONAL_SG \
+            --protocol tcp --port ${secur_gr_egress} \
+            --source-group $CREATE_GET_SECURTY_GROUP_ID &
     done
 }
 
@@ -64,12 +80,16 @@ generate_karpenter_launchtemplate(){
     echo ${karpenter_ct_stack_result}
 }
 
-# aws_auth_apply_configmap() {
-#     aws_auth_configmap_output='aws-auth-output.yml' && rm ${aws_auth_configmap_output}
-#     cp ${aws_auth_configmap_file} ${aws_auth_configmap_output} 
-#     sed -i "s/REPLACE_AWS_ACCOUNT_ID/${AWS_ACCOUNT_ID}/g" ${aws_auth_configmap_output}
-#     kubectl apply -f 
-# }
+aws_auth_apply_configmap() {
+    aws_auth_configmap_output='aws-auth-output.yml' && rm ${aws_auth_configmap_output}
+    cp ${aws_auth_configmap_file} ${aws_auth_configmap_output} 
+    get_nodeinstance_role_name=$(aws iam list-roles | grep -i eksctl | grep -iv service | tail -n1 | cut -f2 -d'/' | tr -d '",')
+    sed -i "s/REPLACE_AWS_ACCOUNT_ID/${AWS_ACCOUNT_ID}/g;\
+    s/REPLACTE_CLUSTER_NAME/${CLUSTER_NAME}/g;\
+    s/REPLACE_NODEINSTANCE_ROLE/${get_nodeinstance_role_name}/g" ${aws_auth_configmap_output}
+    kubectl apply -f ${aws_auth_configmap_output}
+}
 
 apply_provisioner_and_deployment
 generate_karpenter_launchtemplate ${karpenter_launchtemp_template_file}
+aws_auth_apply_configmap
--- get_ec2_spot_instance_ids.sh
+++ get_ec2_spot_instance_ids.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
+cluster_yaml_file='cluster_output.yaml'
 CLUSTER_NAME=$(cat ${cluster_yaml_file}| grep -A1 metadata | tail -n1 | awk '{ print $(NF)}')
 tag_value="karpenter.sh/${CLUSTER_NAME}"
-all_spot_instances=$(aws ec2 describe-instances --filters "Name=instance-lifecycle,Values=spot")
+all_spot_instances=$(aws ec2 describe-instances --region us-east-2 \
+    --filters "Name=instance-lifecycle,Values=spot")
 echo ${all_spot_instances} | jq -r '.Reservations[].Instances[]|select(.Tags[].Value=="'${tag_value}'")|.InstanceId'
 # echo ${all_spot_instances} | jq -r '.Reservations[].Instances[]|select(.Tags[].Value=="'${tag_value}'")|.PrivateIpAddress'
 
